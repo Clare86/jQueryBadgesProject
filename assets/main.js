4fdc74490e9dd15697a8b828e933bb8b78d21ae0
Clare Louise Munson <clarelouise86@btinternet.com>
2017-08-20 11:15:57 +0100

    First

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
   // your code will go here
+    reportCard = "https://www.codeschool.com/users/clare86.json";
+    
+    $.ajax({
+    url: reportCard,
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+        completed = response.courses.completed;
+        for(var i = 0; i < completed.length; i++){
+            $("#badges").append("<div class='course'><h3>" + completed[i].title + "</h3><img src='" + completed[i].badge + "'></img><a href='" + completed[i].url + "' target='_blank' class='btn btn-primary'>See Course</a></div>");
+        }
+    }
+  });
 
 });
